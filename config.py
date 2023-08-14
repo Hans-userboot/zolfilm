b173a38ca9cefc150f58f61c81acb9953a127abe
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2023-08-14 17:41:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,10 +36,10 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://qvrzirmc:uEJJE0pP73cPAFT0YK4QJ-O08pqtn7It@rajje.db.elephantsql.com/qvrzirmc")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://nbzhhlsn:9M7gtoZ8q6OfOCcYCzI6aMcrmkAMJzRj@stampy.db.elephantsql.com/nbzhhlsn")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001400994247"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001749502910"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
